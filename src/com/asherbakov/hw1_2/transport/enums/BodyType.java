cefc343c54d209afd474c1a9ae977c1b7639d703
Anton Scherbakov <ashcherbakov.work@yandex.ru>
2022-10-29 18:16:12 +0900

    initial commit

--- src/com/asherbakov/hw1_2/transport/enums/BodyType.java
+++
@@ -1,7 +0,0 @@
-package com.asherbakov.hw1_2.transport.enums;
-
-public enum BodyType {
-    Sedan,
-    SUV,
-    Pickup
-}
--- src/com/asherbakov/hw1_2/transport/enums/TiresType.java
+++
@@ -1,6 +0,0 @@
-package com.asherbakov.hw1_2.transport.enums;
-
-public enum TiresType {
-    Summer,
-    Winter
-}
